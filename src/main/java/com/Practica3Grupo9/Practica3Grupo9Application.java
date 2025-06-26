cf3dc43e7fdb71905cd0de6adb0fb54df9af8187
valve <valve@EVA-01>
2025-06-25 19:29:57 -0600

    Inicializacion de la Tarea

---
+++ src/main/java/com/Practica3Grupo9/Practica3Grupo9Application.java
@@ -0,0 +1,13 @@
+package com.Practica3Grupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3Grupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3Grupo9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practica3Grupo9/Practica3Grupo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica3Grupo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3Grupo9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
